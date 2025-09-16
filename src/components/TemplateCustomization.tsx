56598afc502b866fe77074d47c906e62687f5073
furaha48 <furahadesign48@gmail.com>
2025-09-16 19:17:22 +0200

    Updated TemplateCustomization.tsx

--- src/components/TemplateCustomization.tsx
+++ src/components/TemplateCustomization.tsx
@@ -350,12 +350,12 @@ const TemplateCustomization = ({ template, onBack, onSave }: TemplateCustomizati
               </div>
 
               <div
-                onClick={() => handleInputChange('backgroundImage', 'https://i.pinimg.com/564x/c0/df/e5/c0dfe50322757f7143c6e2480c9c25df.jpg')}
+                onClick={() => handleInputChange('backgroundImage', 'https://i.weddinghero.ca/gallery/5458/preview_sunam-events_qMCdiF1y.jpg')}
                 className="cursor-pointer group"
               >
                 <div className="relative h-20 rounded-xl overflow-hidden border-2 border-transparent group-hover:border-amber-400 transition-all duration-300">
                   <img
-                    src="https://i.pinimg.com/564x/c0/df/e5/c0dfe50322757f7143c6e2480c9c25df.jpg"
+                    src="https://i.weddinghero.ca/gallery/5458/preview_sunam-events_qMCdiF1y.jpg"
                     alt="Template 3"
                     className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
                   />
@@ -365,12 +365,12 @@ const TemplateCustomization = ({ template, onBack, onSave }: TemplateCustomizati
               </div>
 
               <div
-                onClick={() => handleInputChange('backgroundImage', 'https://images.pexels.com/photos/1729808/pexels-photo-1729808.jpeg?auto=compress&cs=tinysrgb&w=1200')}
+                onClick={() => handleInputChange('backgroundImage', 'https://esdomera.com/cdn/shop/files/unique-leaf-wedding-band-art-deco-gemstone-bridal-ring-14k-rose-gold-unique-stacking-matching-ring-esdomera-1.jpg?v=1744014709')}
                 className="cursor-pointer group"
               >
                 <div className="relative h-20 rounded-xl overflow-hidden border-2 border-transparent group-hover:border-amber-400 transition-all duration-300">
                   <img
-                    src="https://images.pexels.com/photos/1729808/pexels-photo-1729808.jpeg?auto=compress&cs=tinysrgb&w=400"
+                    src="https://esdomera.com/cdn/shop/files/unique-leaf-wedding-band-art-deco-gemstone-bridal-ring-14k-rose-gold-unique-stacking-matching-ring-esdomera-1.jpg?v=1744014709"
                     alt="Template 4"
                     className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
                   />
