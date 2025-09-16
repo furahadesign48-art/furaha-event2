20b22f9878d2b8df8d00e687c0887ecd66cd8d0c
furaha48 <furahadesign48@gmail.com>
2025-09-16 17:43:09 +0200

    Updated GuestExportModal.tsx

--- src/components/GuestExportModal.tsx
+++ src/components/GuestExportModal.tsx
@@ -417,8 +417,8 @@ const confirmedGuests = guests.reduce((sum, g) => sum + (g.confirmed ? guestCoun
 <div>
   • {guests.reduce((sum, g) => sum + (g.confirmed ? guestCount(g) : 0), 0)} confirmé
   {guests.reduce((sum, g) => sum + (g.confirmed ? guestCount(g) : 0), 0) > 1 ? 's' : ''}
+</div 
 </div>
-
               ) : (
                 (() => {
                   const selectedTableData = tablesWithGuests.find(t => t.name === selectedTable);
