03a4f15267e2971d130f0dbf575bd0b0c2e551a9
furaha48 <furahadesign48@gmail.com>
2025-09-16 19:09:14 +0200

    Updated TemplateCustomization.tsx

--- src/components/TemplateCustomization.tsx
+++ src/components/TemplateCustomization.tsx
@@ -320,12 +320,12 @@ const TemplateCustomization = ({ template, onBack, onSave }: TemplateCustomizati
 
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
               <div
-                onClick={() => handleInputChange('backgroundImage', 'https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=1200')}
+                onClick={() => handleInputChange('backgroundImage', 'https://images.pexels.com/photos/29895188/pexels-photo-29895188/free-photo-of-traditional-african-wedding-couple-embrace.jpeg')}
                 className="cursor-pointer group"
               >
                 <div className="relative h-20 rounded-xl overflow-hidden border-2 border-transparent group-hover:border-amber-400 transition-all duration-300">
                   <img
-                    src="https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=400"
+                    src="https://https://images.pexels.com/photos/29895188/pexels-photo-29895188/free-photo-of-traditional-african-wedding-couple-embrace.jpeg"
                     alt="Template 1"
                     className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
                   />
