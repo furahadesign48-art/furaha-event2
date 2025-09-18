b9d7a7eb25256e37152bbf657fc5fcc8ba360028
furaha48 <furahadesign48@gmail.com>
2025-09-18 15:21:20 +0200

    Updated TemplateCustomization.tsx

--- src/components/TemplateCustomization.tsx
+++ src/components/TemplateCustomization.tsx
@@ -222,21 +222,6 @@ const handleSave = async () => {
   }
 };  // 👈 ici tu fermes bien la fonction
 
-const renderTabContent = () => {
-  switch (activeTab) {
-    case "colors":
-      return <div>Éditeur des couleurs</div>;
-    case "fonts":
-      return <div>Éditeur des polices</div>;
-    case "layout":
-      return <div>Éditeur de mise en page</div>;
-    default:
-      return <div>Sélectionnez un onglet</div>;
-  }
-};
-
-
-
   const renderTabContent = () => {
     switch (activeTab) {
       case 'general':
